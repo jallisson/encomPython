751e95c8ed204d4a6e338b865890c2cfb3a8ea3f
jallisson <jallissonn@hotmail.com>
2021-07-23 10:01:45 -0300

    atualização env

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jallis~1\desktop\sistem~2\si447b~1\encomp~4\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
