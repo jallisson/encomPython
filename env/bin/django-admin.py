66af0b86cb20e9c8d6ed6c0fb425757c0be5cdb4
jallisson <jallissonn@hotmail.com>
2022-08-20 16:48:54 -0300

    exclusao env

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/mnt/c/Users/jalli/OneDrive/Área de Trabalho/SISTEMAS JJ/sistema python/acai/encomPython/env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
